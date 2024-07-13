432df65dfb3e94c3b0584487287f8d45089cd12d
Jeffrey Stevens <51374986+JeffreyRStevens@users.noreply.github.com>
2024-07-13 14:44:18 -0500

    Add model selection functions

---
+++ R/model_selection.R
@@ -0,0 +1,112 @@
+
+
+#' Extracts model formula and fixed and random effects when present
+#'
+#' @param x Model object
+#' @param all Logical specifying whether to return random and fixed effects
+#'
+#' @return
+#' Returns list of model formula and random and fixed effects
+#' @export
+#'
+#' @examples
+#' mod <- lm(mpg ~ disp, data = mtcars)
+#' build_formula(mod)
+build_formula <- function(x, all = FALSE) {
+  form <- insight::find_formula(x)
+  dv_fixed <- form$conditional |>
+    deparse()
+  fixed <- dv_fixed |>
+    gsub(".*~ ","",x = _)
+  random <- form$random
+  if(!is.null(random)) {
+    if(inherits(random, "list")) {
+      random <- random |>
+        purrr::list_flatten() |>
+        as.character()
+    } else {
+      random <- random |>
+        deparse()
+    }
+    random <- random |>
+      sub("~", "", x = _) |>
+      paste0("(", x = _, ")", collapse = " + ")
+    formula <- paste(dv_fixed, random, sep = " + ")
+  } else {
+    formula <- dv_fixed
+  }
+  if(all) {
+    output <- list(formula = formula, fixed = fixed, random = random)
+  } else {
+    output <- formula
+  }
+  return(output)
+}
+
+#' Generate AICc, BIC, and Bayes factors for model comparison
+#'
+#' @param ... A list of model objects to compare
+#' @param reference Character string for model to use as reference in model
+#' comparison
+#'
+#' @return
+#' Returns a table with models, AICc, BIC, and Bayes factors.
+#'
+#' @export
+#'
+#' @examples
+#' mod1 <- lm(mpg ~ disp, data = mtcars)
+#' mod2 <- lm(mpg ~ disp + cyl, data = mtcars)
+#' compare_models(mod1, mod2)
+
+compare_models <- function(..., reference = 1) {
+  models <- list(...)
+  names <- unlist(lapply(substitute(list(...))[-1], deparse))
+  names_df <- data.frame(Names = names)
+  comp_models <- performance::compare_performance(...)
+  test_models <- performance::test_performance(..., reference = reference)
+  model_formulas <- models |>
+    purrr::map_chr(build_formula) |>
+    data.frame(Formula = _)
+  model_table <- dplyr::left_join(comp_models, test_models,
+                                  by = c("Name", "Model")) |>
+    dplyr::bind_cols(names_df) |>
+    dplyr::bind_cols(model_formulas) |>
+    dplyr::select(Name = Names, Formula, Model, AICc, BIC, BF)
+  return(model_table)
+}
+
+
+#' Determines best fitting model
+#'
+#' @param x Data frame output from `compare_models()`.
+#' @param threshold Bayes factor threshold used to determine whether best
+#' fitting model is better than second best fitting model.
+#'
+#' @return
+#' Returns list with best fitting model object, model name, model formula, and fixed and random effects.
+#' @export
+#'
+#' @examples
+#' mod1 <- lm(mpg ~ disp, data = mtcars)
+#' mod2 <- lm(mpg ~ disp + cyl, data = mtcars)
+#' mod_comp <- compare_models(mod1, mod2)
+#' find_best_model(mod_comp)
+find_best_model <- function(x, threshold = 3) {
+  first_model <- x$Name[1]
+  sorted_models <- x |>
+    dplyr::arrange(BIC)
+  if(sorted_models$BF[1] >= threshold) {
+    best_model_name <- sorted_models$Name[1]
+  } else {
+    best_model_name <- x$Name[1]
+  }
+  best_model <- eval(parse(text = best_model_name), envir = parent.frame(3))
+  best_form <- build_formula(best_model, all = TRUE)
+  formula <- best_form$formula
+  fixed <- best_form$fixed
+  random <- best_form$random
+  output <- list(best_model = best_model, best_model_name = best_model_name, formula = formula, fixed = fixed, random = random)
+  return(output)
+}
+

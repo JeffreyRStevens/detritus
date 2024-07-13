2f911cd4e7c5afc092405d6a0b737d23d91f03a5
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2024-07-13 12:42:12 -0500

    Add initial validation tests

---
+++ R/validation.R
@@ -0,0 +1,75 @@
+
+#' Test that vector has a maximum number of elements
+#'
+#' @param x Vector
+#' @param num Numeric scalar of maximum number of elements
+#'
+#' @return
+#' Error if maximum number is exceeded; otherwise, nothing.
+#'
+#' @export
+#'
+#' @examples
+test_element_max <- function(x, num) {
+  if(length(x) > num) {
+    cli::cli_abort(paste0(
+      "`{substitute(x)}` must have no more than {num} element{?s}."
+    ))
+  }
+}
+
+#' Test that vector has a minimum number of elements
+#'
+#' @param x Vector
+#' @param num Numeric scalar of minimum number of elements
+#'
+#' @return
+#' Error if minimum number is not met; otherwise, nothing.
+#'
+#' @export
+#'
+#' @examples
+test_element_min <- function(x, num) {
+  if(length(x) < num) {
+    cli::cli_abort(paste0(
+      "`{substitute(x)}` must have at least {num} element{?s}."
+    ))
+  }
+}
+
+#' Test that vector has exact number of elements
+#'
+#' @param x Vector
+#' @param num Numeric scalar of number of elements
+#'
+#' @return
+#' Error if exact number is met; otherwise, nothing.
+#'
+#' @export
+#'
+#' @examples
+test_element_num <- function(x, num) {
+  if(length(x) != num) {
+    cli::cli_abort(
+      "`{substitute(x)}` must have {num} element{?s}, not {length(x)} element{?s}."
+    )
+  }
+}
+
+#' Test if vector is of a specific type
+#'
+#' @param x Vector
+#' @param type Character string of vector type
+#'
+#' @return
+#' Error if type does not match; otherwise, nothing.
+#' @export
+#'
+#' @examples
+test_type <- function(x, type) {
+  if(!inherits(x, type)) {
+    cli::cli_abort(paste0(
+      "`{substitute(x)}` must be of type {type};  not {typeof(x)}."
+    ))
+  }
+}

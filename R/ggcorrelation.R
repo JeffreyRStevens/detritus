f504ebfac886827a09395cb2b6656199d264bad4
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2024-07-13 12:41:58 -0500

    Add axis_prop()

--- R/ggcorrelation.R
+++ R/ggcorrelation.R
@@ -45,36 +45,57 @@ ggcorrelation <- function(df, x, y, xlabel = NULL, ylabel = NULL, cor_type = "pe
   xside <- ifelse(side == "right", 0.85, 0.15)
   corr <- correlation::correlation(df2, select = c("x", "y"), method = cor_type, bayesian = TRUE)
   df2 <- dplyr::mutate(df2, y = as.numeric(as.character(y)),
-                n = corr$n_Obs,
-                r = corr$rho,
-                lowci = corr$CI_low,
-                highci = corr$CI_high,
-                bf = corr$BF)
+                       n = corr$n_Obs,
+                       r = corr$rho,
+                       lowci = corr$CI_low,
+                       highci = corr$CI_high,
+                       bf = corr$BF)
 
   # Plot data and linear regression
   corrplot <- df2 |>
-    ggplot2::ggplot(ggplot2::aes(x = plotx, y = ploty)) +
+    ggplot2::ggplot(ggplot2::aes(x = "plotx", y = "ploty")) +
     ggplot2::geom_point(shape = 1, size = 3) +  # plot individual data points
     ggplot2::geom_smooth(method = "lm", formula = y ~ x) +  # plot regression line
     ggplot2::labs(x = xlabel, y = ylabel) +  # label x- and y-axis
     ggplot2::geom_text(x = axis_prop(df2$plotx, xside),
-              y =  axis_prop(df2$ploty, 0.9),
-              label = paste("n = ", df2$n, "\n",
-                            stat_var, " = ", cocoon::format_num(df2$r, 2), "\n",
-                            # format_p(df2$p, digits = 3, pzero = TRUE, italics = FALSE), "\n",
-                            cocoon::format_bf(df2$bf, cutoff = 1000, italics = FALSE, subscript = "")), size = 6) +  # add p-values and Bayes factors
+                       y =  axis_prop(df2$ploty, 0.9),
+                       label = paste("n = ", df2$n, "\n",
+                                     stat_var, " = ", cocoon::format_num(df2$r, 2), "\n",
+                                     # format_p(df2$p, digits = 3, pzero = TRUE, italics = FALSE), "\n",
+                                     cocoon::format_bf(df2$bf, cutoff = 1000, italics = FALSE, subscript = "")), size = 6) +  # add p-values and Bayes factors
     # {if(study) facet_wrap(vars(study))} +
     ggplot2::theme_bw() +  # change theme
     ggplot2::theme(#text = ggplot2::element_text(family = "Arial"),
-          panel.grid = ggplot2::element_blank(),  # remove grid lines
-          axis.title = ggplot2::element_text(size = 20),  # set axis label font size
-          axis.text = ggplot2::element_text(size = 15),
-          aspect.ratio = 1)  # set tick mark label font size
+      panel.grid = ggplot2::element_blank(),  # remove grid lines
+      axis.title = ggplot2::element_text(size = 20),  # set axis label font size
+      axis.text = ggplot2::element_text(size = 15),
+      aspect.ratio = 1)  # set tick mark label font size
   output <- list(plot = corrplot, coefficient = corr$rho, bf = corr$BF)  # create list of output
   return(output)
 }
 
-axis_prop <- function(vec, prop) {
+#' Find a fixed proportion between the minimum and maximum values in a vector
+#'
+#' @description
+#' This function finds the minimum and maximum values in a vector then
+#' calculates a requested proportion of that. It is used to place text in
+#' plots.
+#'
+#'
+#' @param vec Numeric vector of data values
+#' @param prop Numeric scalar of proportion to be calculated
+#'
+#' @return
+#' Scalar value of calculated proportion
+#' @export
+#'
+#' @examples
+axis_prop <- function(vec, prop = 0.5) {
+  test_type(vec, type = "numeric")
+  test_type(prop, type = "numeric")
+  test_element_min(vec, 2)
+  test_element_num(prop, 1)
   prop * (max(vec, na.rm = TRUE) - min(vec, na.rm = TRUE)) + min(vec, na.rm = TRUE)
 }
+
 

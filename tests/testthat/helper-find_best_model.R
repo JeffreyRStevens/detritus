4f2700b6c6862bacf63c4abf12bd6df65344dc66
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2024-08-13 17:52:25 -0500

    Clean up merge

--- tests/testthat/helper-find_best_model.R
+++
@@ -1,8 +0,0 @@
-model1 <- lm(mpg ~ 1, data = mtcars)
-# model2 <- lm(mpg ~ disp, data = mtcars)
-model3 <- lm(mpg ~ disp + hp, data = mtcars)
-# model4 <- lm(mpg ~ disp * hp, data = mtcars)
-model5 <- lme4::lmer(mpg ~ 1 + (1 | gear), data = mtcars)
-# model6 <- lme4::lmer(mpg ~ hp + (1 | gear), data = mtcars)
-model7 <- lme4::lmer(mpg ~ hp + (1 | gear) + (1 | carb), data = mtcars)
-

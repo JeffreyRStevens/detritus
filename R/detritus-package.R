d59a75e3da0f91670b0ca49b6dc204b35b936e7d
Jeffrey Stevens <51374986+JeffreyRStevens@users.noreply.github.com>
2025-01-19 18:24:48 -0600

    Add dependencies

--- R/detritus-package.R
+++ R/detritus-package.R
@@ -3,6 +3,9 @@
 
 ## usethis namespace: start
 #' @importFrom lme4 lmer
+#' @importFrom stats as.formula
 #' @importFrom stats BIC
+#' @importFrom stats binomial
+#' @importFrom stats lm
 ## usethis namespace: end
 NULL
